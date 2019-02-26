33cd08a0c7753e3e5a782f7e6c055234bd03c561
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-02-26 17:33:59 +0100

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-#export MONGO_DB_HOST==$(echo $PS_HOSTS | awk -F ':' '{print $1}')
-#export MONGO_DB_PORT=27017
-#export EXPERIMENT_NAME="howdy"
-
 apt-get update -y
 apt-get install -y netcat
 
@@ -11,18 +7,6 @@ echo "Copying objective function to hyerpopt worker dir"
 MONGO_WORKER_PATH=$(dirname $(which hyperopt-mongo-worker))
 cp ./cifar10_tutorial.py $MONGO_WORKER_PATH
 
-if [[ $JOB_NAME == "ps" ]]; then
-    # install mongo and open up port
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-    apt-get update
-    apt-get install -y mongodb-org
-    mongod --dbpath . --port $MONGO_DB_PORT --directoryperdb --fork --journal --logpath log.log --nohttpinterface
-
-    echo "[+] Im a PS running Mongo"
-else
-    echo "[+] Im a worker ready for action..."
-fi
 
 while :; do
     if nc -z $MONGO_DB_HOST $MONGO_DB_PORT 2>/dev/null; then
