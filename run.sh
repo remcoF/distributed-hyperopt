e3e58cb47853ca5bb49f812638190790e1631651
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-04-03 14:22:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,14 +7,16 @@ echo "Copying objective function to hyerpopt worker dir"
 MONGO_WORKER_PATH=$(dirname $(which hyperopt-mongo-worker))
 cp ./cifar10_tutorial.py $MONGO_WORKER_PATH
 
+echo $MONGO_DB_HOST $MONGO_DB_PORT
 
 while :; do
-    if nc -z $MONGO_DB_HOST $MONGO_DB_PORT 2>/dev/null; then
+    if nc -z -v $MONGO_DB_HOST $MONGO_DB_PORT 2>/dev/null; then
         echo "mongodb is up!"
         hyperopt-mongo-worker --mongo=$MONGO_DB_HOST:$MONGO_DB_PORT/foo_db --poll-interval=1.0 --exp-key=$EXPERIMENT_NAME &
         python ./tune_cifar10.py
         exit
     else
+        nc -z -v $MONGO_DB_HOST $MONGO_DB_PORT
         echo "mongodb is down!"
         sleep 5
     fi
