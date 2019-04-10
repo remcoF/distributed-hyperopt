f7d93931b18acdb566d9ec3b29d6464f26011842
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-04-10 16:02:54 +0200

    fixed run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,7 @@ echo $MONGO_DB_HOST $MONGO_DB_PORT
 while :; do
     if nc -z -v $MONGO_DB_HOST $MONGO_DB_PORT 2>/dev/null; then
         echo "mongodb is up!"
-        hyperopt-mongo-worker --mongo=$MONGO_DB_HOST:$MONGO_DB_PORT/foo_db --poll-interval=1.0 --exp-key=distributed-hyperopt
- &
+        hyperopt-mongo-worker --mongo=$MONGO_DB_HOST:$MONGO_DB_PORT/foo_db --poll-interval=1.0 --exp-key=distributed-hyperopt &
         python ./tune_cifar10.py
         exit
     else
