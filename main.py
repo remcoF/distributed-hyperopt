61f37b001d6d52579a7f21bb907a5d03ad104646
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-04-10 16:15:36 +0200

    fixed logging in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import os
 import json
 import math
+import logging
 from time import sleep
 from hyperopt import fmin, tpe, hp
 from hyperopt.mongoexp import MongoTrials
@@ -24,8 +25,10 @@ def main():
 
     while True:
         try:
+            logging.debug('Launching MongoTrials for {}'.format(experiment_name))
             trials = MongoTrials(mongo_connect_str, exp_key=experiment_name)
         except ServerSelectionTimeoutError:
+            logging.debug('No MongoDB server is available for an operation')
             pass
         else:
             space = {
@@ -36,8 +39,9 @@ def main():
             if os.environ["TYPE"] == "ps":
                 save_path = os.path.join(data_dir, "results.json")
                 with open(save_path, "w") as f:
+                    logging.debug('Saving results.json to {}'.format(data_dir))
+                    logging.info('Results: {}'.format((str(best))))
                     json.dump(json.dumps(best), f)
-                    print(str(best))
             return
 
 
